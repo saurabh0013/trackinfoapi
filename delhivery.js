7a88fe0f0dc26b091fdec0f6960c6002d99478b3
saurabh <saurabh.sps.13@gmail.com>
2023-08-17 16:33:44 +0530

    data mapping changed

--- delhivery.js
+++ delhivery.js
@@ -24,18 +24,18 @@ async function track_delhivery(Id){
   let place;
   let comment;
   var len;
-        if(response.data[0].receiverName != 'null'){
+        if(response.data[0].message == 'Success'){
            
          
          
          len  =  response.data[0].scans.length;
          for (let i =len-1; i>=0;i--){
-         status =  response.data[0].scans[i].status;
-         dateTime = response.data[0].scans[i].scanDateTime;
+         status =  response.data[0].status[i].status;
+         dateTime = response.data[0].status[i].statusDateTime;
          place=response.data[0].scans[i].scannedLocation;
          date =  dateTime.slice(0,10);
          time = dateTime.slice(11,16);
-         comment =response.data[0].scans[i].instructions;
+         comment =response.data[0].status[i].instructions;
         
          trackingData.push({
                 
